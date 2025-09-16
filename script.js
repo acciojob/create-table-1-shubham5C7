deae4ecdf902297a90c72bc908a3db6c646efffd
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-16 15:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+
+const table = document.getElementById("sampleTable");
+
 function insert_Row() {
-    //Write your code here
-  
-  
+   
+     const tr = document.createElement("tr");
+   const td1 = document.createElement("td");
+   td1.innerText = "New Cell1"
+
+   const td2 = document.createElement("td");
+   td2.innerText = "New Cell2"
+    tr.append(td1,td2);
+     table.appendChild(tr);
+    table.insertBefore(tr, table.firstChild);
 }
